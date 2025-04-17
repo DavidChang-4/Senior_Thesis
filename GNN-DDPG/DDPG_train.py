4491a18373f099760a4db6d526dab5d757306a20
DavidChang-4 <113307995+DavidChang-4@users.noreply.github.com>
2025-04-17 02:30:06 -0400

    Update DDPG_train.py with the partial observability version

--- GNN-DDPG/DDPG_train.py
+++ GNN-DDPG/DDPG_train.py
@@ -13,6 +13,9 @@ import time
 teams = ['pursuer', 'evader']
 teams_training = ['pursuer', 'evader']
 
+def outside_observable(source, target, OBS_RADIUS=0.8):
+    return torch.norm(source - target) > OBS_RADIUS
+
 ####################################
 # Training Loop
 ####################################
@@ -29,22 +32,46 @@ def train(env, MAX_CYCLES,
     N_PURSUERS, N_EVADERS = len(p_names), len(e_names)
 
     # Track rewards
-    reward_history = []
+    NUM_TRIALS = NUM_SAMPLES_COLLECT // MAX_CYCLES
+    reward_hist, catch_percent_hist, first_catch_hist, connection_hist = [], [], [], []
     time_accum = 0
 
     for episode in range(NUM_EPISODES):
         start_time = time.perf_counter()
-        num_samples_collected = total_reward = 0
+        num_samples_collected = 0
+        total_reward = trials_caught = 0
+        first_catch_time_accum = 0
+        sample_connected = 0
 
         # Stage 1: Collect experience
         while num_samples_collected < NUM_SAMPLES_COLLECT:
             observations, _ = env.reset() # Reset env
             graphs = GraphEncoder(p_names, e_names, observations)
+            uncaught_flag = True
 
+            t = 0
             while True:
                 # 1.1: Convert observations to tensors
                 p_obs = torch.tensor(np.array([observations[p].copy() for p in p_names]))
                 e_obs = torch.tensor(np.array([observations[e].copy() for e in e_names]))
+
+                # Delete observations if outside of pursuer observability
+                # Pursuer 0: evader
+                outside = 0
+                if outside_observable(e_obs[0][2:4], p_obs[0][2:4]):
+                    p_obs[0][6:] = torch.zeros(4)
+                    outside+=1
+                # Pursuer 1: evader
+                if outside_observable(e_obs[0][2:4], p_obs[1][2:4]):
+                    p_obs[1][6:] = torch.zeros(4)
+                    outside+=1
+                if outside_observable(p_obs[0][2:4], p_obs[1][2:4]):
+                    p_obs[0][4:6] = torch.zeros(2)
+                    p_obs[1][4:6] = torch.zeros(2)
+                    outside+=1
+                if outside!=3:
+                    sample_connected += 1
+                    
 
                 # 1.2: Get actions from actor networks
                 p_actions = actors['pursuer'](p_obs, graphs.p_graph).detach().numpy() # Get pursuer actions. Shape (num_pursuer, 2)
@@ -64,7 +91,14 @@ def train(env, MAX_CYCLES,
                 # 1.3: Take step in environment and observe result 
                 next_observations, rewards, terminations, truncations, _ = env.step(env_actions)
                 done = terminations[p_names[0]] or truncations[p_names[0]]
+
+                # Logging
+                t+=1
                 total_reward += rewards[p_names[0]] # Track total reward in this rollout. 
+                if uncaught_flag and rewards[p_names[0]]>0:
+                    uncaught_flag = False
+                    trials_caught += 1
+                    first_catch_time_accum += t
 
                 # Convert next observations to tensors
                 p_obs_next = torch.tensor(np.array([next_observations[p].copy() for p in p_names]))
@@ -82,6 +116,9 @@ def train(env, MAX_CYCLES,
                 p_next_graph = graphs.p_graph.clone()
                 e_next_graph = graphs.e_graph.clone()
 
+                if len(graphs.p_graph.edge_attr.shape) != 1: # No connections
+                    sample_connected += 1
+
                 # 1.3: store transition in RB
                 buffers['pursuer'].push(p_obs, p_obs_next, p_actions, p_rewards, p_done, p_old_graph, p_next_graph) 
                 buffers['evader'].push(e_obs, e_obs_next, e_actions, e_rewards, e_done, e_old_graph, e_next_graph) # TODO: evaders have their own graph
@@ -89,10 +126,16 @@ def train(env, MAX_CYCLES,
 
                 # Check 'done', otherwise update observations
                 if done:
+                    if uncaught_flag:
+                        first_catch_time_accum += 100
                     break
                 observations = next_observations
 
-        mean_episode_reward = total_reward / (NUM_SAMPLES_COLLECT // MAX_CYCLES)
+        # Logging
+        mean_ep_reward = total_reward / (NUM_TRIALS)
+        catch_percent = trials_caught / (NUM_TRIALS)
+        mean_first_catch_t = (first_catch_time_accum / NUM_TRIALS)
+        mean_connection = sample_connected / NUM_SAMPLES_COLLECT
 
         # Stage 2: Learn from collected samples.
         for _ in range(NUM_OPTIM_STEPS):
@@ -130,16 +173,22 @@ def train(env, MAX_CYCLES,
                     target_param.data.copy_(TAU * source_param.data + (1.0 - TAU) * target_param.data)
         
         # Remove evader from training
-        if episode == (NUM_EPISODES // 3) and len(teams_training)>1:
+        if episode == (NUM_EPISODES//3) and len(teams_training)>1:
             teams_training.remove('evader')
-
 
         # Report statistics
         episode_time = time.perf_counter() - start_time
         time_accum += episode_time
-        print(f"Episode {episode+1} | Avg Reward: {mean_episode_reward:.0f} | Time: {episode_time:.2f} | Avg Time: {(time_accum/(episode+1)):.2f}")
-        reward_history.append(mean_episode_reward)
-        np.save("GNN-DDPG/rewards", reward_history)
+        print(f"Ep {episode+1} | Avg Reward: {mean_ep_reward:.0f} | Catch %: {catch_percent:.2f}| 1st Catch T: {mean_first_catch_t:.0f} | Connection %: {mean_connection:.2f}| Avg T: {(time_accum/(episode+1)):.2f}")
+        reward_hist.append(mean_ep_reward)
+        catch_percent_hist.append(catch_percent)
+        first_catch_hist.append(mean_first_catch_t)
+        connection_hist.append(mean_connection)
+
+        np.save("GNN-DDPG/log_rewards", reward_hist)
+        np.save("GNN-DDPG/log_catch_percent", catch_percent_hist)
+        np.save("GNN-DDPG/log_catch_t", first_catch_hist)
+        np.save("GNN-DDPG/log_connection_t", connection_hist)
         
         # Save models every 10 episodes. 
         if (episode+1) % 10 == 0:
