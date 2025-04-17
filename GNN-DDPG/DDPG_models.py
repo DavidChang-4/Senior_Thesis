b01be858046ff8fb9d46fc33b203cd89329146fe
DavidChang-4 <113307995+DavidChang-4@users.noreply.github.com>
2025-04-17 02:31:24 -0400

    Update DDPG_models.py with a K=2 model

--- GNN-DDPG/DDPG_models.py
+++ GNN-DDPG/DDPG_models.py
@@ -8,21 +8,20 @@ from torch_geometric.data import Data
 
 import numpy as np
 
-from egnn_gcl import GCL, E_GCL, E_GCL_vel, GCL_rf_vel
-
 ACTION_DIM = 2 # [vertical, horizontal]
 
 ####################################
 # Actor
 ####################################
-class Actor(nn.Module):
+class Actor_GNN(nn.Module):
     def __init__(self, team, N_PURSUERS, N_EVADERS, in_dim, mlp_hidden_dim=512):
-        super(Actor, self).__init__()
+        super(Actor_GNN, self).__init__()
         self.team = team
         self.p, self.e = N_PURSUERS, N_EVADERS
         
         # GNN Approach
-        self.gnn = GATConv(5, 128, heads=4, concat=True, aggr="max")
+        self.gnn1 = GATConv(5, 128, heads=4, concat=True, aggr="max")
+        self.gnn2 = GATConv(128*4, 128, heads=4, concat=True, aggr="max")
         self.mlp1 = nn.Sequential(
             nn.Linear(128*4, mlp_hidden_dim),
             nn.ReLU(),
@@ -31,18 +30,14 @@ class Actor(nn.Module):
             nn.Linear(mlp_hidden_dim, ACTION_DIM), # Output 1 value
             nn.Tanh()
         )
-
-        # MLP Approach
-        # self.mlp = nn.Sequential(
-        #     nn.Linear(in_dim, mlp_hidden_dim), # changed from 128
-        #     nn.ReLU(),
-        #     nn.Linear(mlp_hidden_dim, mlp_hidden_dim),
-        #     nn.ReLU(),
-        #     nn.Linear(mlp_hidden_dim, mlp_hidden_dim), # remove layer when doing GNN
-        #     nn.ReLU(),
-        #     nn.Linear(mlp_hidden_dim, ACTION_DIM), # Output action
-        #     nn.Tanh() # map to [-1, 1]
-        # )
+        self.mlp2 = nn.Sequential(
+            nn.Linear(5, mlp_hidden_dim),
+            nn.ReLU(),
+            nn.Linear(mlp_hidden_dim, mlp_hidden_dim),
+            nn.ReLU(),
+            nn.Linear(mlp_hidden_dim, ACTION_DIM), # Output 1 value
+            nn.Tanh()
+        )
 
     def forward(self, observation, graph, batch_size=1):
         # GNN Team Setup
@@ -52,14 +47,38 @@ class Actor(nn.Module):
         team_idx = pursuer_idx if self.team=="pursuer" else evader_idx # Indices for agents in team
 
         # GNN Stage
-        node_embeddings = self.gnn(graph.x, graph.edge_index, graph.edge_attr) # GNN Stage
-        mlp1_out = self.mlp1(node_embeddings[team_idx])
+        if len(graph.edge_attr > 0):
+            node_embeddings = self.gnn1(graph.x, graph.edge_index, graph.edge_attr) # GNN Stage
+            node_embeddings = self.gnn2(node_embeddings, graph.edge_index, graph.edge_attr)
+            mlp_out = self.mlp1(node_embeddings[team_idx])
+        else:
+            mlp_out = self.mlp2(graph.x[team_idx])
 
         if batch_size>1: 
-            mlp1_out = mlp1_out.view(batch_size, team_size, 2)
+            mlp_out = mlp_out.view(batch_size, team_size, 2)
         
-        return mlp1_out
+        return mlp_out
 
+        # MLP Implementation
+        return self.mlp(observation)
+    
+class Actor_MLP(nn.Module):
+    def __init__(self, team, N_PURSUERS, N_EVADERS, in_dim, mlp_hidden_dim=512):
+        super(Actor_MLP, self).__init__()
+
+        # MLP Approach
+        self.mlp = nn.Sequential(
+            nn.Linear(in_dim, mlp_hidden_dim), # changed from 128
+            nn.ReLU(),
+            nn.Linear(mlp_hidden_dim, mlp_hidden_dim),
+            nn.ReLU(),
+            nn.Linear(mlp_hidden_dim, mlp_hidden_dim), # remove layer when doing GNN
+            nn.ReLU(),
+            nn.Linear(mlp_hidden_dim, ACTION_DIM), # Output action
+            nn.Tanh() # map to [-1, 1]
+        )
+
+    def forward(self, observation, graph, batch_size=1):
         # MLP Implementation
         return self.mlp(observation)
 
